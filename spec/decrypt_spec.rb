ead4c89d5098b5e9f44fa6c76c641c4977771df8
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 15:07:32 -0600

    add test for collection of characters

--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -9,4 +9,8 @@ describe Decrypt do
     expect(@decrypt).to be_instance_of Decrypt
   end
 
+  it '2. has a collection of characters' do
+    expect(@decrypt.characters).to eq ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+  end
+
 end
