b5c8db4e809b4e27b70b5fe3c62bb1e5216a83fe
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 17:20:57 -0600

    add encrypt and decrypt for both date and random key

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -18,7 +18,7 @@ describe Enigma do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq expected
   end
 
-  it '3. decrypts a message with a key and date' do
+  it 'decrypts a message with a key and date' do
     expected = {
       decryption: "hello world",
       key: "02715",
@@ -26,5 +26,14 @@ describe Enigma do
     }
     expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq expected
   end
-  
+
+  it 'encrypt and decrypt using todays date' do
+    encrypted = @enigma.encrypt("hello world", "02715")
+    expect(@enigma.decrypt(encrypted[:encryption], "02715")[:decryption]).to eq "hello world"
+  end
+
+  it 'encrypt and decrypt using random key' do
+    encrypted = @enigma.encrypt("hello world")
+    expect(@enigma.decrypt(encrypted[:encryption], encrypted[:key])[:decryption]).to eq "hello world"
+  end
 end
