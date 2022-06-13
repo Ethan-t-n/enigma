e9333f494011f355ddbc6c257d4bbe650f867896
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 12:30:24 -0600

    finish initialize methods for now

--- lib/methods_store.rb
+++ lib/methods_store.rb
@@ -12,10 +12,10 @@ class MethodsStore
   def initialize(key, date)
     generate_characters
     assign_date(date)
-    assign_key(key) unless self.class == Crack
-    generate_shift_keys unless self.class == Crack
-    generate_offsets unless self.class == Crack
-    generate_shifts unless self.class == Crack
+    assign_key(key)
+    generate_shift_keys
+    generate_offsets
+    generate_shifts
   end
 
   def generate_characters
@@ -24,6 +24,28 @@ class MethodsStore
 
   def assign_date(date)
     date == nil ? @date = todays_date : @date = date
+  end
+
+  def assign_key(key)
+    key == nil ? @key = random_key : @key = key
+  end
+
+  def generate_shift_keys
+    @shift_keys = {A: @key[0..1].to_i, B: @key[1..2].to_i, C: @key[2..3].to_i, D: @key[3..4].to_i}
+  end
+
+  def generate_offsets
+    last_4 = (@date.to_i**2).to_s[-4..-1]
+    @offsets = {A: last_4[0].to_i, B: last_4[1].to_i, C: last_4[2].to_i, D: last_4[3].to_i}
+  end
+
+  def generate_shifts
+    @shifts = {
+      A: @shift_keys[:A] + @offsets[:A],
+      B: @shift_keys[:B] + @offsets[:B],
+      C: @shift_keys[:C] + @offsets[:C],
+      D: @shift_keys[:D] + @offsets[:D]
+    }
   end
 
 end
