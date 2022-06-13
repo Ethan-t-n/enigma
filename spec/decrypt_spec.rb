5c1b328ff3ff5d42304f3736804eaaed96a750af
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 15:16:39 -0600

    add test for keys, offsets, and shifts

--- spec/decrypt_spec.rb
+++ spec/decrypt_spec.rb
@@ -4,7 +4,8 @@ describe Decrypt do
   before(:each) do
     @decrypt = Decrypt.new("keder ohulw", "02715", "040895")
   end
-
+  #I like this, just copy and paste from encrypt
+  #with a few changes.
   it 'exists' do
     expect(@decrypt).to be_instance_of Decrypt
   end
@@ -26,10 +27,29 @@ describe Decrypt do
     expect(decrypt.date).to eq expected
   end
 
-  it 'a random key' do
+  it 'has a random key' do
     decrypt = Decrypt.new("keder ohulw")
     expect(decrypt.key.class).to be String
     expect(decrypt.key.length).to be 5
+  end
+
+  it 'has shift keys' do
+    expected = {A: 2, B: 27, C: 71, D: 15}
+    expect(@decrypt.shift_keys).to eq expected
+  end
+
+  it 'has offsets' do
+    expected = {A: 1, B: 0, C: 2, D: 5}
+    expect(@decrypt.offsets).to eq expected
+  end
+
+  it 'has shifts' do
+    expected = {A: 3, B: 27, C: 73, D: 20}
+    expect(@decrypt.shifts).to eq expected
+  end
+
+  it '8. decrypts the message' do
+    expect(@decrypt.decrypt).to eq "hello world"
   end
 
 end
