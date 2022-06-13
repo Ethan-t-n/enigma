81204d919133deef9458038b70dd4fef15aa2cb3
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 12:16:28 -0600

    add test for collection of characters

--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -8,4 +8,8 @@ describe Encrypt do
   it 'exists' do
     expect(@encrypt).to be_instance_of Encrypt
   end
+
+  it '2. has a collection of characters' do
+    expect(@encrypt.characters).to eq ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+  end
 end
