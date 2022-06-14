cd944d09b4f30af62afbedb008a5f2cd8542af88
Ethan Nguyen <etnguyen16@gmail.com>
2022-06-13 21:14:12 -0600

    add argument vector files for decrypt and encrypt

---
+++ decrypt.rb
@@ -0,0 +1,21 @@
+require './helper.rb'
+
+if ARGV.count != 4 ||
+  !File.exist?(ARGV[0]) ||
+  (ARGV[1].include?("/") && !File.exist?(ARGV[1].split("/")[0..-2].join("/"))) ||
+  ARGV[2].length != 5 ||
+  ARGV[3].length != 6 ||
+  ARGV[2].to_i.to_s.rjust(5, "0") != ARGV[2] ||
+  ARGV[3].to_i.to_s.rjust(6, "0") != ARGV[3]
+
+  puts "INVALID INPUT\n- Make sure your source file is valid.\n- Make sure to use a valid filepath for your output.\n- The key must be exactly 5 digits, numbers only.\n- Enter a date using 'DDMMYY' formatting. 6 digits, numbers only.\nCOMMAND STRUCTURE:\n$ ruby decrypt.rb <source filepath> <output filepath> <key> <date>"
+  exit #I dont know if this is the right syntax. I saw an example on stack overflow
+end
+
+ciphertext = File.read(ARGV[0]).to_s
+decrypted = Enigma.new.decrypt(ciphertext, ARGV[2], ARGV[3])
+
+File.new(ARGV[1], "w+")
+File.write(ARGV[1], decrypted[:decryption])
+
+puts "Created #{ARGV[1]} with the key #{decrypted[:key]} and date #{decrypted[:date]}"
